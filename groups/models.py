71ca48201dea0e2c46f8057e0f6ca563c341a8d6
Dima Kasperovich <dimakasperovich1720@gmail.com>
2021-11-27 19:04:46 +0200

    django lms project start

---
+++ groups/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Group(models.Model):
+
+    cours_name = models.CharField(max_length=30)
+    start_date = models.DateField(default="19.10.2021")
+    number_of_students = models.IntegerField()
+    teacher_name = models.CharField(max_length=50)
