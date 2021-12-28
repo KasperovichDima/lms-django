f68d567f7e485938ccb5eccfe94bddf778451e6c
Dima Kasperovich <dimakasperovich1720@gmail.com>
2021-12-28 18:10:34 +0200

    homework 20

---
+++ accounts/views.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+import django.views.generic as CBV
+
+# Create your views here.
+from django.urls import reverse_lazy, reverse
+
+from accounts.forms import AccountRegistrationForm
+
+
+class AccountRegistrationView(CBV.CreateView):
+    model = User
+    template_name = 'accounts/registration.html'
+    success_url = reverse_lazy('index')
+    form_class = AccountRegistrationForm
+
+
+class AccountLoginView(LoginView):
+    template_name = 'accounts/login.html'
+
+    def get_redirect_url(self):
+        next_value = self.request.GET.get('next')
+        if next_value:
+            return next_value
+
+        return reverse('index')
+
+
+class AccountLogoutView(LogoutView):
+    template_name = 'accounts/logout.html'
