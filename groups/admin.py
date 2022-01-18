5f117d3f1fdf5f25d82be6eb42b0bad2dc86a683
Dima Kasperovich <dimakasperovich1720@gmail.com>
2022-01-18 09:11:40 +0200

    homework 21

--- groups/admin.py
+++ groups/admin.py
@@ -3,10 +3,11 @@ from django.contrib import admin
 from groups.models import Group
 
 from students.models import Student
+# from teachers.models import Teacher
 
 
 class StudentsInlineTable(admin.TabularInline):
-    extra = 5
+    extra = 2
     model = Student
     fields = [
         'first_name',
@@ -15,14 +16,30 @@ class StudentsInlineTable(admin.TabularInline):
         'phone_number',
         'group',
     ]
-    readonly_fields = fields
-    show_change_link = True
+    readonly_fields = []
+    show_change_link = False
+
+
+class TeachersInlineTable(admin.TabularInline):
+    model = Group.teachers.through
+    # fields = [
+    #     'first_name',
+    #     'last_name',
+    #     'birthday',
+    #     'specialization',
+    #     'work_experience',
+    # ]
 
 
 class GroupAdmin(admin.ModelAdmin):
-    list_display = ['group_name', 'start_date', 'headman']
-    fields = ['group_name', 'start_date', 'headman', 'teachers']
-    inlines = [StudentsInlineTable]
+    list_display = ['group_name', 'start_date', 'headman', ]
+    fields = ['group_name',
+              'start_date',
+              'headman',
+              'teachers'
+              ]
+    inlines = [TeachersInlineTable, StudentsInlineTable]
+    filter_horizontal = ['teachers']
 
 
 admin.site.register(Group, GroupAdmin)
--- teachers/admin.py
+++ teachers/admin.py
@@ -1,3 +1,30 @@
-from django.contrib import admin   # noqa
+from teachers.models import Teacher
 
-# Register your models here.
+from django.contrib import admin
+
+
+class TeacherAdmin(admin.ModelAdmin):
+    fields = [
+        ('first_name', 'last_name'),
+        ('birthday', 'age'),
+        ('specialization', 'work_experience'),
+        'phone_number',
+        'salary',
+    ]
+    readonly_fields = ['age']
+    list_display = ['first_name',
+                    'last_name',
+                    'birthday',
+                    'specialization',
+                    'work_experience',
+                    ]
+    list_display_links = list_display
+    list_per_page = 20
+    search_fields = ['first_name', 'last_name', 'specialization']
+    list_filter = (
+        'specialization',
+        'work_experience'
+    )
+
+
+admin.site.register(Teacher, TeacherAdmin)
